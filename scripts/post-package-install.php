d6a55c1af992f029a847e625bbfd173071ad5a53
Jess Archer <jess@jessarcher.com>
2023-12-11 09:43:22 +1000

    wip

--- scripts/post-package-install.php
+++ scripts/post-package-install.php
@@ -3,6 +3,10 @@
 use function Laravel\Prompts\info;
 use function Laravel\Prompts\text;
 
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    exit(0);
+}
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 info('post-package-install');
--- scripts/post-root-package-install.php
+++ scripts/post-root-package-install.php
@@ -3,10 +3,6 @@
 use function Laravel\Prompts\info;
 use function Laravel\Prompts\text;
 
-if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    exit(0);
-}
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 info('post-root-package-install');
